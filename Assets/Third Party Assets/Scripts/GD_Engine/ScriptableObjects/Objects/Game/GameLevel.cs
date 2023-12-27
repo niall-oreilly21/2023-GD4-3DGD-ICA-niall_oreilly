b17dcc30c58e4a107dfa7f93b3b3c5739237d7ec
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-27 20:00:57 +0000

    :bug: Add Odin inspector to fix issues in GD_Engine scripts

--- Assets/Third Party Assets/Scripts/GD_Engine/ScriptableObjects/Objects/Game/GameLevel.cs
+++ Assets/Third Party Assets/Scripts/GD_Engine/ScriptableObjects/Objects/Game/GameLevel.cs
@@ -12,42 +12,42 @@ namespace GD
     [CreateAssetMenu(fileName = "GameLevel", menuName = "DkIT/Scriptable Objects/Game/Level", order = 2)]
     public class GameLevel : ScriptableGameObject
     {
-        [TabGroup("tab1", "Scenes", SdfIconType.PieChartFill)]
-        [Searchable]
-        public List<GameScene> Scenes;
-
-        [TabGroup("tab1", "Objectives (optional)", SdfIconType.CardChecklist)]
-        [Searchable]
-        public List<GameObjective> Objectives;
-
-        [TabGroup("tab1", "Postprocessing (optional)", SdfIconType.Fan)]
-        public Volume PostProcessPrefab;
-
-        [TabGroup("tab1", "Postprocessing (optional)")]
-        public VolumeProfile DefaultPostProcessProfile;
-
-        //internal
-        private Volume instancePostProcessPrefab;
-
-        public void LoadLevel()
-        {
-            foreach (GameScene scene in Scenes)
-                scene.LoadScene();
-
-            if (PostProcessPrefab != null && DefaultPostProcessProfile != null)
-            {
-                instancePostProcessPrefab = Instantiate(PostProcessPrefab);
-                instancePostProcessPrefab.profile = DefaultPostProcessProfile;
-            }
-        }
-
-        public void UnloadLevel()
-        {
-            foreach (GameScene scene in Scenes)
-                scene.UnloadScene();
-
-            if (instancePostProcessPrefab != null)
-                Destroy(instancePostProcessPrefab);
-        }
+    [TabGroup("tab1", "Scenes", SdfIconType.PieChartFill)]
+    [Searchable]
+    public List<GameScene> Scenes;
+    
+    [TabGroup("tab1", "Objectives (optional)", SdfIconType.CardChecklist)]
+    [Searchable]
+    public List<GameObjective> Objectives;
+    
+    [TabGroup("tab1", "Postprocessing (optional)", SdfIconType.Fan)]
+    // public Volume PostProcessPrefab;
+    //
+    // [TabGroup("tab1", "Postprocessing (optional)")]
+    // public VolumeProfile DefaultPostProcessProfile;
+    //
+    // //internal
+    // private Volume instancePostProcessPrefab;
+    
+    public void LoadLevel()
+    {
+        foreach (GameScene scene in Scenes)
+            scene.LoadScene();
+    
+        // if (PostProcessPrefab != null && DefaultPostProcessProfile != null)
+        // {
+        //     instancePostProcessPrefab = Instantiate(PostProcessPrefab);
+        //     instancePostProcessPrefab.profile = DefaultPostProcessProfile;
+        // }
+    }
+    
+    public void UnloadLevel()
+    {
+        foreach (GameScene scene in Scenes)
+            scene.UnloadScene();
+    
+        // if (instancePostProcessPrefab != null)
+        //     Destroy(instancePostProcessPrefab);
+    }
     }
 }
