1dc8c3836d72f76ae7497fdf10161146fc76ea67
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-28 20:24:39 +0000

    :sparkles: Add Game Event and Game Event Listener for Multi-Lingual Game Object

---
+++ Assets/My Assets/Scripts/MultiLanguageGameEvent.cs
@@ -0,0 +1,13 @@
+using GD;
+using My_Assets.Scripts.ScriptableObjects;
+using UnityEngine;
+
+namespace My_Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "ItemDataGameEvent",
+        menuName = "DkIT/Scriptable Objects/Events/MultiLingualObject")]
+    public class MultiLanguageGameEvent :BaseGameEvent<MultiLingualObject>
+    {
+
+    }
+}
---
+++ Assets/My Assets/Scripts/MultiLanguageGameEventListener.cs
@@ -0,0 +1,9 @@
+using GD;
+using My_Assets.Scripts.ScriptableObjects;
+
+namespace My_Assets.Scripts
+{
+    public class MultiLanguageGameEventListener : BaseGameEventListener<MultiLingualObject>
+    {
+    }
+}
