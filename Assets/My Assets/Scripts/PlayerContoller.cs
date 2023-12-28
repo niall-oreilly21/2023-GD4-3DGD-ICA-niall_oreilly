1350676e0fc20c3920135e41060eea84e5952d0a
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-28 20:09:52 +0000

    :sparkles: Add Advanced Selection Manager

--- Assets/My Assets/Scripts/PlayerContoller.cs
+++ Assets/My Assets/Scripts/PlayerContoller.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using GD.Controllers;
 using UnityEngine;
 
-public class PlayerContoller : MonoBehaviour
+public class PlayerContoller : CharacterNavigationController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+        private static readonly string IS_RUNNING = "isRunning";
         
+        protected override void SetAnimationWhenGoingToDestination()
+    {
+        animator.SetBool(IS_RUNNING, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void SetAnimationWhenReachedDestination()
     {
-        
+        animator.SetBool(IS_RUNNING, false);
     }
 }
