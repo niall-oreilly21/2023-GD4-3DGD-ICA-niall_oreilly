facb249d9ff895ce1f8eb1f5230222372129ddab
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-28 20:14:50 +0000

    :sparkles: Add Inventory Manager and Inventory script
    This will be used to store the items the player has collected.

---
+++ Assets/My Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,27 @@
+using My_Assets.Scripts.ScriptableObjects;
+using UnityEngine;
+
+namespace My_Assets.Scripts.Managers
+{
+    public class InventoryManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Inventory inventory;
+
+        public void HandleConsumablePickup(MultiLingualObject item)
+        {
+            if (inventory.Contents.ContainsKey(item))
+            {
+                int count = inventory.Contents[item];
+                count++; //since int is value type this is a copy so we need to rewrite in
+                inventory.Contents[item] = count;
+                // inventory.Contents.Add(item, count);
+            }
+            else
+            {
+                inventory.Contents.Add(item, 1);
+            }
+        }
+    }
+}
+
---
+++ Assets/My Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace My_Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Inventory",
+            menuName = "DkIT/Scriptable Objects/Game/Inventory")]
+        public class Inventory : SerializedScriptableObject
+        {
+            public Dictionary<MultiLingualObject, int> Contents;
+        }
+}
