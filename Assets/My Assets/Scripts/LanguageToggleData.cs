b912a8073fb9b3e55045359ca33cd606291fb79a
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-31 16:26:39 +0000

    :sparkles: Add Language Toggle Data class

---
+++ Assets/My Assets/Scripts/LanguageToggleData.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace My_Assets.Scripts
+{
+    [System.Serializable]
+    public class LanguageToggleData
+    {
+        [SerializeField]
+        private LanguagesToLearnType language;
+        
+        [SerializeField]
+        private Toggle toggle;
+        
+        public LanguageToggleData()
+        {
+        }
+
+        public LanguagesToLearnType Language
+        {
+            get { return language; }
+            set { language = value; }
+        }
+        
+        public Toggle Toggle
+        {
+            get { return toggle; }
+            set { toggle = value; }
+        }
+    }
+}
