b03a057296336fee0521478c5a1c61cac2936d48
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-31 16:29:03 +0000

    :hammer: Update Level Preference Controller to update the language picked

--- Assets/My Assets/Scripts/Behaviours/SetLevelPreferencesController.cs
+++ Assets/My Assets/Scripts/Behaviours/SetLevelPreferencesController.cs
@@ -1,38 +1,73 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using GD;
 using My_Assets.Scripts.ScriptableObjects;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SetLevelPreferencesController : MonoBehaviour
+namespace My_Assets.Scripts.Behaviours
 {
-    [SerializeField]
-    private TMP_InputField languageToLearn;
-    
-    [SerializeField]
-    private Slider countOfWordsToLearn;
-    
-    [SerializeField]
-    private Slider countOfWordsToTest;
-    
-    [SerializeField]
-    private InputField  tutorialSelected;
-    
-    [SerializeField]
-    private LevelPreferencesData  levelPreferencesData;
-
-    void Update()
+    public class SetLevelPreferencesController : MonoBehaviour
     {
-        UpdateLevelPreferences();
-    }
+        [SerializeField]
+        private Slider countOfWordsToLearn;
+    
+        [SerializeField]
+        private Slider countOfWordsToTest;
 
-    private void UpdateLevelPreferences()
-    {
-        levelPreferencesData.LanguageToLearn = languageToLearn.text;
-        levelPreferencesData.CountOfWordsToLearn = (int)countOfWordsToLearn.value;
-        levelPreferencesData.CountOfWordsToTest = (int)countOfWordsToTest.value;
-        //levelPreferencesData.TutorialSelected = tutorialSelected.text;
+        [SerializeField]
+        private InputField  tutorialSelected;
+    
+        [SerializeField]
+        private LevelPreferencesData  levelPreferencesData;
+
+        [SerializeField]
+        private List<LanguageToggleData> toggles;
+        
+        private LanguageToggleData currentToggle;
+
+        private void Start()
+        {
+            currentToggle = new LanguageToggleData();
+        }
+
+        void Update()
+        {
+            UpdateLevelPreferences();
+        }
+        public void SetLanguageToLearn(string languageToLearn)
+        {
+            var languagesToLearnType = languageToLearn.Equals("Spanish") ? LanguagesToLearnType.Spanish : LanguagesToLearnType.French;
+            
+            if (currentToggle.Language.Equals(languagesToLearnType))
+            {
+                return;
+            }
+            
+            foreach (var toggle in toggles.Where(toggle => toggle.Language.Equals(languagesToLearnType)))
+            {
+                
+                // Disable the currentToggleSelected
+                if (currentToggle.Toggle != null)
+                {
+                    currentToggle.Toggle.isOn = false;
+                }
+
+                // Update the languageToLearn in levelPreferencesData
+                
+                levelPreferencesData.LanguageToLearn = languagesToLearnType;
+
+                currentToggle = toggle;
+            }
+        }
+        
+        private void UpdateLevelPreferences()
+        {
+            levelPreferencesData.CountOfWordsToLearn = (int)countOfWordsToLearn.value;
+            levelPreferencesData.CountOfWordsToTest = (int)countOfWordsToTest.value;
+        }
     }
 }
