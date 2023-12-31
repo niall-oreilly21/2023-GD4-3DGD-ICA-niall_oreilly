6fdca69228cbd6ee03715197a4b413982eb36706
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2023-12-31 22:26:38 +0000

    :sparkles: Add bool Game Event and Game Event listener

---
+++ Assets/My Assets/Scripts/ScriptableObjects/Events/BoolGameEvent.cs
@@ -0,0 +1,11 @@
+using GD;
+using UnityEngine;
+
+namespace My_Assets.Scripts.ScriptableObjects.Events
+{
+    [CreateAssetMenu(fileName = "BoolGameEvent", menuName = "DkIT/Scriptable Objects/Patterns/Events/Bool")]
+    public class BoolGameEvent: BaseGameEvent<bool>
+    {
+        
+    }
+}
---
+++ Assets/My Assets/Scripts/ScriptableObjects/Events/BoolGameEventListener.cs
@@ -0,0 +1,9 @@
+using GD;
+
+namespace My_Assets.Scripts.ScriptableObjects.Events
+{
+    public class BoolGameEventListener : BaseGameEventListener<bool>
+    {
+        
+    }
+}
