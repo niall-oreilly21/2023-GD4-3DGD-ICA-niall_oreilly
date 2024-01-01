4e8d1deabd3f7f45343e847855c746410a7b6271
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2024-01-01 15:44:36 +0000

    :sparkles: Add method GetCountOfCorrectWordsOnShoppingList()

--- Assets/My Assets/Scripts/ScriptableObjects/Inventory.cs
+++ Assets/My Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace My_Assets.Scripts.ScriptableObjects
 {
-    [CreateAssetMenu(fileName = "Inventory",
-            menuName = "DkIT/Scriptable Objects/Game/Inventory")]
+    [CreateAssetMenu(fileName = "Inventory", menuName = "DkIT/Scriptable Objects/Game/Inventory")]
         public class Inventory : SerializedScriptableObject
         {
             public Dictionary<string, MultiLingualData> Contents;
+            
+            public int GetCountOfCorrectWordsOnShoppingList()
+            {
+                return Contents.Values.Count(item => item.WordIsToBeTested);
+            }
         }
 }
