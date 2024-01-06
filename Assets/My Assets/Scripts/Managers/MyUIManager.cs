5729e792523defb25b52231713ca18efc8b71c6c
niall-oreilly21 <91060319+niall-oreilly21@users.noreply.github.com>
2024-01-06 01:44:17 +0000

    :truck: Delete and renamed Assets folder

--- Assets/My Assets/Scripts/Managers/MyUIManager.cs
+++ Assets/My Assets/Scripts/Managers/MyUIManager.cs
@@ -117,42 +117,33 @@ namespace My_Assets.Scripts.Managers
         /// <param name="totalWordsToLearn">Total words to learn in the level.</param>
         public void DisplayEndMenu(List<MultiLingualData> itemsNotInInventory, int totalWordsToLearn)
         {
-            int endScore = DisplayInventoryEndCart();
+            DisplayInventoryEndCart();
             DisplayItemsNotInInventory(itemsNotInInventory);
-            UpdateScoreText(endScore, totalWordsToLearn);
+            UpdateScoreText(totalWordsToLearn);
             UnHideEndScreen();
         }
 
         /// <summary>
         /// Updates the score text in the end menu.
         /// </summary>
-        /// <param name="endScore">The score achieved.</param>
         /// <param name="totalWordsToLearn">Total words to learn in the level.</param>
-        private void UpdateScoreText(int endScore, int totalWordsToLearn)
+        private void UpdateScoreText(int totalWordsToLearn)
         {
             endScoreText.SetText("Score " + "\n" + inventory.GetCountOfCorrectWordsOnShoppingList() + " / " + totalWordsToLearn);
         }
         
         /// <summary>
-        /// Displays the inventory items in the end menu cart and calculates the end score.
+        /// Displays the inventory items in the end menu cart.
         /// </summary>
-        /// <returns>The end score achieved.</returns>
-        private int DisplayInventoryEndCart()
+        private void DisplayInventoryEndCart()
         {
             var sortedItems = inventory.Contents.Values.OrderBy(item => item.EnglishLanguageData.LanguageText);
-            int endScore = 0;
-            
+
             foreach (MultiLingualData item in sortedItems)
             {
                 var newItem = DisplayEndMenuCartItem(item.EnglishLanguageData.LanguageText, item.CurrentLanguageToLearnData.LanguageText, endMenuInventoryUIComponent.ItemContent);
                 newItem.transform.Find("Icon").GetComponent<Image>().sprite = item.WordIsToBeTested ? tickIcon : xIcon;
-
-                if (item.WordIsToBeTested)
-                {
-                    endScore++;
-                }
             }
-            return endScore;
         }
 
         /// <summary>
